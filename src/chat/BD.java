83bb451994ba6093b45066e117df79ca9657bfc0
Oseias Ayres Ferreira <oseias_ayres@hotmail.com>
2018-09-18 11:12:52 -0300

    alter table and foreign key in database

--- src/chat/BD.java
+++ src/chat/BD.java
@@ -38,48 +38,30 @@ public class BD {
         } 
         
     }
+    
+    
     public void insertUser()
     {
         
     }
+    
+    
     public void insertChat(String senderId, String receiverId, String msgContent, String date) 
     {
         int p1 = 1;
         int p2 = 2;
         
         
-        try {
-            PreparedStatement id = conn.prepareStatement("SELECT id_user FROM users WHERE name=?");
-            id.setString(1,senderId);
-//            System.out.println("Inicio Select");
-            ResultSet rs= id.executeQuery();
-            if(rs.next())
-            {
-//                System.out.println("entrei");
-                p1 = rs.getInt("id_user");
-//                System.out.println("P1: "+ p1);
-            }
-            else
-                return;
-            
-            id.setString(1,receiverId);
-            rs= id.executeQuery();
-            if(rs.next())
-            {
-                p2 = rs.getInt("id_user");
-//                System.out.println("P2: "+ p2);
-            }
-            else
-                return;
-            id.close();
-            System.out.println("Insertion");
-            PreparedStatement q = conn.prepareStatement("INSERT INTO CHAT (sender,receiver,content,dt) VALUES(?,?,?,?)");
-            q.setInt(1,p1);
-            q.setInt(2,p2);
-            q.setString(3,msgContent);
-            q.setString(4,date);
+        try
+        {
+            PreparedStatement q = conn.prepareStatement("INSERT INTO CHAT (sender, receiver, content, dt) VALUES(?,?,?,?)");
+            q.setString(1, senderId);
+            q.setString(2, receiverId);
+            q.setString(3, msgContent);
+            q.setString(4, date);
             q.execute();
             q.close();
+            
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
@@ -87,6 +69,7 @@ public class BD {
         
     }
 
+    
     public ArrayList<Chatter> getChatter(String senderId, String receiverId) {
 
         ArrayList<Chatter> content = new ArrayList<>();
@@ -94,18 +77,19 @@ public class BD {
         try {
             // select content,dt from chat c where c.sender in (SELECT id_user FROM users where name = 'marco' or name = 'ze') and c.receiver in (SELECT id_user FROM users where name = 'ze' or name = 'marco' ) order by dt; 
 //            System.out.println("Init getChatter");
-            PreparedStatement id = conn.prepareStatement("select sender,receiver,content,dt from chat c where c.sender in (SELECT id_user FROM users where name = ? or name = ?) and c.receiver in (SELECT id_user FROM users where name = ? or name = ? ) order by dt");
+            PreparedStatement id = conn.prepareStatement("select * from chat c where c.sender in (?, ?) and c.receiver in (?, ?) order by dt");
             
-            id.setString(1,senderId);
-            id.setString(2,receiverId);
-            id.setString(3,senderId);
-            id.setString(4,receiverId);
+            id.setString(1, senderId);
+            id.setString(2, receiverId);
+            id.setString(3, senderId);
+            id.setString(4, receiverId);
           
            
-            ResultSet rs= id.executeQuery();
+            ResultSet rs = id.executeQuery();
             while(rs.next())
             {   
-                Chatter obj = new Chatter(senderId,receiverId,rs.getString("content"),rs.getString("dt")); 
+                Chatter obj = new Chatter(rs.getString("sender"), rs.getString("receiver"),
+                        rs.getString("content"), rs.getString("dt")); 
                 content.add(obj);
                 
             }
